1fbccf42408d0870924c7960d8f600e8a4f02f55
martin aliverti <martin.aliverti@gmail.com>
2020-06-19 15:56:16 -0300

    Better ux to add tasks

--- frontend/src/screens/tasks/NewTask.jsx
+++ frontend/src/screens/tasks/NewTask.jsx
@@ -3,10 +3,13 @@ import moment from "moment";
 import DateFnsUtils from "@date-io/date-fns";
 import { TextField, Button } from "@material-ui/core";
 import { MuiPickersUtilsProvider, DatePicker } from "@material-ui/pickers";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/DeleteForever";
 import { useTasks } from "../../hooks/useTasks";
 import useForm from "../../hooks/useForm";
 
 export default () => {
+  const [collapsed, setCollapsed] = useState(true);
   const [date, setDate] = useState(moment());
   const { createTask } = useTasks();
   const [task, handleChange] = useForm({
@@ -17,43 +20,74 @@ export default () => {
   const reset = () => {
     handleChange({ target: { value: "", name: "text" } });
     setDate(moment());
+    setCollapsed(true);
   };
   const create = () => {
     createTask({ ...task, dueDate: date });
     reset();
   };
-  return (
-    <div style={styles.container}>
-      <TextField
-        variant="outlined"
-        value={task.text}
-        onChange={handleChange}
-        name="text"
-        multiline
-      />
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <DatePicker
-          variant="inline"
-          format="yyyy/MM/dd"
-          margin="normal"
-          label={"Due date"}
-          value={date}
-          onChange={(newDate) => setDate(moment(newDate))}
-          minDate={moment()}
-        />
-      </MuiPickersUtilsProvider>
-      <Button
-        color="primary"
-        variant="contained"
-        disableElevation
-        style={{ alignSelf: "flex-end", marginTop: 10 }}
-        onClick={create}
-      >
-        Add
-      </Button>
-    </div>
-  );
+  return collapsed
+    ? displayCollapsed({ expand: () => setCollapsed(false) })
+    : displayExpanded({ task, date, setDate, handleChange, create, reset });
 };
+
+const displayCollapsed = ({ expand }) => (
+  <Button
+    color="primary"
+    variant="contained"
+    style={styles.newTaskButton}
+    onClick={expand}
+  >
+    New Task
+  </Button>
+);
+
+const displayExpanded = ({
+  task,
+  date,
+  setDate,
+  handleChange,
+  create,
+  reset,
+}) => (
+  <div style={styles.container}>
+    <IconButton
+      style={{ alignSelf: "flex-end" }}
+      size="small"
+      color="primary"
+      onClick={reset}
+    >
+      <DeleteIcon />
+    </IconButton>
+    <TextField
+      variant="outlined"
+      value={task.text}
+      onChange={handleChange}
+      name="text"
+      multiline
+    />
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <DatePicker
+        variant="inline"
+        format="yyyy/MM/dd"
+        margin="normal"
+        label={"Due date"}
+        value={date}
+        onChange={(newDate) => setDate(moment(newDate))}
+        minDate={moment()}
+      />
+    </MuiPickersUtilsProvider>
+    <Button
+      color="primary"
+      variant="contained"
+      disableElevation
+      style={{ alignSelf: "flex-end", marginTop: 10 }}
+      onClick={create}
+    >
+      Add
+    </Button>
+  </div>
+);
 
 const styles = {
   container: {
@@ -64,5 +98,8 @@ const styles = {
     padding: 10,
     marginBottom: 10,
     width: 250,
+  },
+  newTaskButton: {
+    width: "100%",
   },
 };
