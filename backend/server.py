bb8c58fde10f25f76d30cd3ff8974aa1cdd64030
martin aliverti <martin.aliverti@gmail.com>
2020-06-19 11:15:27 -0300

    WIP - Re-enabled CORS. Still problems with non-auth requests

--- backend/server.py
+++ backend/server.py
@@ -8,8 +8,10 @@ from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token,
     create_refresh_token, get_jwt_identity
 )
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config["DEBUG"] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todos.db"
